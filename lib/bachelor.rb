2d5cf0041e61121d0c8aa4ac41577d1d0d9cb925
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-13 20:21:21 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,77 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+
+  data[season].each do |hashes|
+    if hashes["status"] = "Winner"
+      return hashes["name"].split(" ").first
+    end
+  end
+
 end
+
+
 
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each do |season, info|
+    info.each do |information|
+      if information["occupation"] == occupation
+        return information["name"]
+      end
+    end
+  end
+
 end
 
+
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+
+  counter = 0
+
+  data.each do |season, info|
+    info.each do |information|
+      information.each do |type, datas|
+        if datas == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+
+  return counter
+
 end
 
 def get_occupation(data, hometown)
-  # code here
+
+  data.each do |season, info|
+    info.each do |information|
+      if information["hometown"] == hometown
+        return information["occupation"]
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+
+  age_array = []
+
+  data[season].each do |information|
+    age_array.push(information["age"].to_i)
+  end
+
+  sum = 0
+
+  age_array.each do |num|
+    sum += num
+  end
+
+  avg_age = (sum.to_f/age_array.length.to_f).round
+
+  return avg_age
+
 end
